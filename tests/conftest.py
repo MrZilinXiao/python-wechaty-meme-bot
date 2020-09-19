a1937e6b91b73036e29bc83c623d7d0f6f14ef8d
MrZilinXiao <me@mrxiao.net>
2020-09-19 19:35:29 +0800

    [test] CD Test #5

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,11 +3,14 @@ from backend.response.conversation import ConversationHandler
 from backend.response.dispatcher import DirectHandler
 from backend.dataset import ImportDataset
 from torchvision import transforms
+from backend.utils import ConfigParser
 
 
 # return conversation handler in CPU-only mode
+
 @pytest.fixture(scope='module')
 def conversation_handler():
+    cp = ConfigParser('backend/config.yaml')
     return ConversationHandler('cpu')
 
 
